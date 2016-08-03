4b1d8fc17905729cee21d97e1ee6d55104138004
Jacob Keen <jaclkeen@gmail.com>
2016-08-03 17:59:50 -0500

    converted to card style, event listener now selects only one card, many other changes

--- CarLot.js
+++ CarLot.js
@@ -1,25 +1,25 @@
 function printCar(cars){
-  var display_area = document.querySelector('.t_body');
+  var display_area = document.querySelector('#container');
   cars.cars.forEach(function(element){
     if(element.purchased === false){
-      element.purchased = 'Available'
+      element.purchased = 'Available';
     }
     else{
-      element.purchased = 'Not Available'
+      element.purchased = 'Not Available';
     }
 
-    var vehicle = document.createElement('tr');
+    var vehicle = document.createElement('div');
     vehicle.className = "row";
     vehicle.classList.add(element.color);
     vehicle.innerHTML +=
-          `<td class='data make'>${element.make}</td>
-          <td class='data model'>${element.model}</td>
-          <td class='data year'>${element.year}</td>
-          <td class='data price'>$${element.price}</td>
-          <td class='data color'>${element.color}</td>
-          <td class='data purchased'>${element.purchased}</td>
-          <td class='data description' id='description'>${element.description}</td>`;
-    vehicle.style.border = "3px solid " + element.color;
+          `<h2 class='data make'>${element.make}</h2>
+          <p class='data model'>Model: ${element.model}</p>
+          <p class='data year'>Year: ${element.year}</p>
+          <p class='data price'>Price: $${element.price}</p>
+          <p class='data color'>Color: ${element.color}</p>
+          <p class='data purchased'>Avalability: ${element.purchased}</p>
+          <p class='data description' id='description'>Description: ${element.description}</p>`;
+    vehicle.style.border = "4px solid " + element.color;
     eventListeners(vehicle);
     display_area.appendChild(vehicle);
     carLot.add(vehicle);
@@ -27,25 +27,32 @@ function printCar(cars){
 }
 
 function eventListeners(vehicle){
-  var td = document.querySelector('#t_body');
+  var container = document.querySelector('#container');
   var input = document.getElementById('input');
   var submit = document.getElementById('submit');
 
-  td.addEventListener("dblclick", function(e){
-    if(e.target.id === 'description'){
-      e.target.classList.toggle("selected");
-      input.value = e.target.innerHTML;
-      input.focus();
-    }
       submit.addEventListener("click", function(){
         if(e.target.id === 'description'){
           e.target.innerHTML = input.value;
           e.target.classList.toggle("selected");
         }
       })
-  })
 
   vehicle.addEventListener("click", function(e){
-    e.target.classList.toggle("active");
+    for(var j = 0; j < vehicle.childNodes.length; j++){
+      vehicle.childNodes[j].className = "";
+      var active_p = e.target;
+      active_p.classList.add('active');
+    }
+    if(e.target.classList.contains('active') && e.target.tagName !== "DIV"){
+      input.value = ""
+      input.value = e.target.innerHTML;
+      input.focus();
+    }
+    for(var i = 0; i < container.childNodes.length; i++){
+      container.childNodes[i].className = "row";
+      var selected_card = e.target;
+      selected_card.classList.add('selected');
+    }
   })
 }
