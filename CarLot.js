b11deed585cfcb5ab6e05cde093a5322181f4202
Jacob Keen <jaclkeen@gmail.com>
2016-08-04 13:08:29 -0500

    completed bugs

--- CarLot.js
+++ CarLot.js
@@ -9,7 +9,7 @@ function printCar(cars){
     }
 
     var vehicle = document.createElement('div');
-    vehicle.className = "row card";
+    vehicle.className = "col-md-4 card";
     vehicle.classList.add(element.color);
     vehicle.innerHTML +=
           `<h2 class='data make'>${element.make}</h2>
@@ -20,47 +20,45 @@ function printCar(cars){
           <p class='data purchased'>Avalability: ${element.purchased}</p>
           <p class='description'>Description: ${element.description}</p>`;
     vehicle.style.border = "4px solid " + element.color;
-    eventListeners(vehicle);
+
     display_area.appendChild(vehicle);
     carLot.add(vehicle);
   })
+  eventListeners()
 }
 
-function eventListeners(vehicle){
+function eventListeners(){
   var container = document.querySelector('#container');
   var input = document.getElementById('input');
   var submit = document.getElementById('submit');
-  var children = [];
+  var cars = document.querySelectorAll('.card');
   var targetDescription = null;
 
-  vehicle.addEventListener("click", function(e){
+  for (var i = cars.length - 1; i >= 0; i--) {
+    cars[i].addEventListener("click", function(e){
+      console.log("fullEvent >>>>",e.currentTarget.childNodes)
+      console.log("currentTarget >>>>",e.currentTarget)
+      console.log("target >>>>",e.target)
 
-    console.log("fullEvent >>>>",e.currentTarget.childNodes)
-    console.log("currentTarget >>>>",e.currentTarget)
-    console.log("target >>>>",e.target)
-    // for(var j = 0; j < vehicle.childNodes.length; j++){
-    //   vehicle.childNodes[j].className = "";
-    //   active_p.classList.add('active');
-    // }
-      var active_p = e.currentTarget;
-      targetDescription = active_p.querySelector('.description')
-      console.log(active_p, ":currentTarget , desc:", targetDescription)
-    // if(e.currentTarget.classList.contains('active')){
-      input.value = ""
-      input.value = targetDescription.innerHTML;
-      input.focus();
-    // }
-    for(var i = 0; i < container.childNodes.length; i++){
-      container.childNodes[i].className = "row";
-      var selected_card = e.currentTarget;
-      selected_card.classList.add('selected');
-    }
-  })
+        active_p = e.currentTarget;
+        targetDescription = active_p.querySelector('.description')
+        console.log(active_p, ":currentTarget , desc:", targetDescription)
+
+        input.value = ""
+        input.value = targetDescription.innerHTML;
+        input.focus();
+
+      for(var i = 0; i < container.childNodes.length; i++){
+        container.childNodes[i].className = "col-md-4";
+        var selected_card = e.currentTarget;
+        selected_card.classList.add('selected');
+      }
+    })
+  } // End of for loop
 
   input.addEventListener("input", function(e){
       if (targetDescription !== null) {
         targetDescription.innerHTML = input.value;
       }
-
   })
 }
